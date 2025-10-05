df5b5de7f807fd00c1e4d7ae7ce1e17691bdec8c
Freya Pines <one-true-cat@yandex.ru>
2025-10-05 17:24:15 +0300

    add run_tail func

--- os-lab-3/src/client.c
+++ os-lab-3/src/client.c
@@ -1,11 +1,10 @@
+#include "errno.h"
 #include "stdio.h"
 #include "stdlib.h"
 #include "sys/wait.h"
 #include "unistd.h"
 #include "variables.h"
 #include <string.h>
-#include "errno.h"
-
 
 void die(const char *msg) {
   fprintf(stderr, "%s\n", msg);
@@ -13,7 +12,8 @@ void die(const char *msg) {
   exit(EXIT_FAILURE);
 }
 
-int run_subprocess(const char *path, char *const args[], char* rbuf, int rsize, char* wbuf, int wsize) {
+int run_subprocess(const char *path, char *const args[], const char *rbuf, int rsize,
+                   char *wbuf, int wsize) {
   int child_to_parent[2];
   int parent_to_child[2];
   pid_t pid;
@@ -49,7 +49,6 @@ int run_subprocess(const char *path, char *const args[], char* rbuf, int rsize, 
     }
     close(parent_to_child[1]);
 
-
     wait(NULL);
     int nbytes = read(child_to_parent[0], wbuf, wsize);
     return nbytes;
@@ -58,12 +57,12 @@ int run_subprocess(const char *path, char *const args[], char* rbuf, int rsize, 
   return 1;
 }
 
-int run_subprocess_nopipe(const char *path, char *const args[], char* wbuf, int wsize) {
+int run_subprocess_nopipe(const char *path, char *const args[], char *wbuf,
+                          int wsize) {
   return run_subprocess(path, args, "", 0, wbuf, wsize);
 }
 
-
-int run_find(char* buf, int size) {
+int run_find(char *buf, int size) {
   const char *find_path = "/usr/bin/find";
   char *const find_args[] = {"find", ".", "-name", "*.sh", NULL};
 
@@ -71,13 +70,13 @@ int run_find(char* buf, int size) {
   return res;
 }
 
-int run_wc(char* buf, int size) {
-  const char *wc_path= "/usr/bin/wc";
-  char* wc_args[100] = {"wc", "-l"};
+int run_wc(char *buf, int size) {
+  const char *wc_path = "/usr/bin/wc";
+  char *wc_args[100] = {"wc", "-l"};
   int i = 2;
   char *sep = strtok(buf, "\n");
   while (sep != NULL) {
-    char* arg = malloc(50);
+    char *arg = malloc(50);
     strcpy(arg, sep);
     wc_args[i++] = arg;
     sep = strtok(NULL, "\n");
@@ -90,21 +89,33 @@ int run_wc(char* buf, int size) {
   return res;
 }
 
+int run_tail(const char *rbuf, int rsize, char* wbuf, int wsize) {
+  const char *tail_path = "/usr/bin/tail";
+  char *tail_args[] = {"tail", "-n", "1", NULL};
+
+  const char* test_str = "lol\nkek\ncheburek";
+  int res = run_subprocess(tail_path, tail_args, test_str, sizeof(test_str), wbuf, wsize);
+  return res;
+}
+
 int main() {
 
-  const char *wc_path= "/usr/bin/wc";
-  const char *tail= "/usr/bin/tail";
+  const char *wc_path = "/usr/bin/wc";
+  const char *tail = "/usr/bin/tail";
   char buf[1024];
 
   // Run find to get the files
   int res = run_find(buf, sizeof(buf));
   printf("got (%.*s)\n", res, buf);
 
+  // run wc to get the line count
   res = run_wc(buf, sizeof(buf));
 
+  printf("got (%.*s)\n", res, buf);
+
   // run wc to get the line count
+  const char* rbuf = buf;
+  res = run_tail(rbuf, sizeof(rbuf), buf, sizeof(buf));
 
-
-  
   printf("got (%.*s)\n", res, buf);
 }
