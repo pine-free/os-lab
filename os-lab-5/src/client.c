035ee571b54fae596c4378161b08ecbe24e63fcf
Freya Pines <one-true-cat@yandex.ru>
2025-10-07 20:27:20 +0300

    add get sleeping procs

--- os-lab-5/src/client.c
+++ os-lab-5/src/client.c
@@ -1,5 +1,6 @@
 #include "common.h"
 #include "sockets.h"
+#include <sys/un.h>
 
 #define CLIENT_PRINT(...) INFO_PRINT("client", __VA_ARGS__)
 
@@ -15,6 +16,12 @@ int init_client(struct myclient* client) {
   return 1;
 }
 
+int get_sleeping_procs(char* wbuf, int wsize) {
+  char buf[1024];
+  RUN("/usr/bin/ps", buf, "ps", "-eo", "pid=,s=");
+  RPIPE("/usr/bin/grep", buf, buf, "grep", "S");
+  strncpy(wbuf, buf, wsize);
+}
 
 int main() {
   set_debug();
@@ -23,6 +30,10 @@ int main() {
   struct myclient client;
   init_client(&client);
   OR_DIE(unix_connect(&client.srv_sock));
+
+  char sleeping_procs[1024];
+
+  get_sleeping_procs(sleeping_procs, sizeof(sleeping_procs));
 
   return 0;
 }
