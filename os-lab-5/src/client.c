4e78ab68c57ee72f444df3a8a625fe61ce696e8d
Freya Pines <one-true-cat@yandex.ru>
2025-10-07 18:28:53 +0300

    add client

---
+++ os-lab-5/src/client.c
@@ -0,0 +1,26 @@
+#include "common.h"
+#include "sockets.h"
+
+#define CLIENT_PRINT(...) INFO_PRINT("client", __VA_ARGS__)
+
+struct myclient {
+  struct unix_socket sock;
+};
+
+struct myclient get_client() {
+  if (DEBUG) {
+    DBG_PRINT("create client")
+  }
+  struct myclient client = {get_socket()};
+  return client;
+}
+
+
+int main() {
+  CLIENT_PRINT("starting client");
+
+  struct myclient client = get_client();
+  unix_connect(&client.sock);
+
+  return 0;
+}
