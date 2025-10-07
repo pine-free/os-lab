2f1b9eb9943821f3c1e2e09fef93edd1d75b32df
Freya Pines <one-true-cat@yandex.ru>
2025-10-07 19:02:43 +0300

    add err printing

--- os-lab-5/src/debug.h
+++ os-lab-5/src/debug.h
@@ -7,6 +7,14 @@ extern int DEBUG;
 
 int set_debug();
 
+#define LOG_PRINT_DETAILED(type, buf, ...)                                     \
+  {                                                                            \
+    fprintf(buf, "[%s - %s:%d - %s] ", type, __BASE_FILE__, __LINE__,          \
+            __FUNCTION__);                                                     \
+    fprintf(buf, __VA_ARGS__);                                                 \
+    fprintf(buf, "\n");                                                        \
+  }
+
 #define INFO_PRINT(label, ...)                                                 \
   {                                                                            \
     printf("[INFO - %s] ", label);                                             \
@@ -14,9 +22,5 @@ int set_debug();
     printf("\n");                                                              \
   }
 
-#define DBG_PRINT(...)                                                         \
-  {                                                                            \
-    printf("[DEBUG - %s:%d - %s] ", __BASE_FILE__, __LINE__, __FUNCTION__);    \
-    printf(__VA_ARGS__);                                                       \
-    printf("\n");                                                              \
-  }
+#define DBG_PRINT(...) LOG_PRINT_DETAILED("DEBUG", stdout, __VA_ARGS__)
+#define ERR_PRINT(...) LOG_PRINT_DETAILED("ERROR", stderr, __VA_ARGS__)
