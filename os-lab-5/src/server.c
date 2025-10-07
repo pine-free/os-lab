0dc402842a88b1cf80547eb21c2151ebc594289b
Freya Pines <one-true-cat@yandex.ru>
2025-10-07 18:40:43 +0300

    add server

---
+++ os-lab-5/src/server.c
@@ -0,0 +1,30 @@
+#include "common.h"
+#include "sockets.h"
+
+#define SERVER_PRINT(...) INFO_PRINT("server", __VA_ARGS__)
+
+struct myserver {
+  struct unix_socket sock;
+};
+
+struct myserver get_server() {
+  if (DEBUG) {
+    DBG_PRINT("create server");
+  }
+  struct myserver server = {get_socket()};
+}
+
+int main() {
+  SERVER_PRINT("starting server");
+  struct myserver server = get_server();
+  unix_bind(&server.sock);
+  unix_listen(&server.sock);
+
+  struct unix_socket client_sock = get_socket();
+
+  while(1) {
+    unix_accept(&server.sock, &client_sock);
+  }
+
+  
+}
