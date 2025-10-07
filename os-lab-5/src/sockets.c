1bda55adb899cec0c16aba22d44000a6386d468e
Freya Pines <one-true-cat@yandex.ru>
2025-10-07 18:40:41 +0300

    add unix_accept func

--- os-lab-5/src/sockets.c
+++ os-lab-5/src/sockets.c
@@ -4,7 +4,7 @@
 
 int get_sockfd() {
   int sock = socket(S_DOMAIN, S_DOMAIN, S_PROTOCOL);
-  return sock;  
+  return sock;
 }
 
 struct sockaddr_un get_sockaddr() {
@@ -21,6 +21,7 @@ struct unix_socket get_socket() {
   struct unix_socket sock;
   sock.sfd = get_sockfd();
   sock.addr = get_sockaddr();
+  sock.addr_size = sizeof(sock.addr);
   return sock;
 }
 
@@ -28,13 +29,13 @@ int unix_bind(struct unix_socket *sock) {
   if (DEBUG) {
     DBG_PRINT("binding socket to addr %s", sock->addr.sun_path);
   }
-  bind(sock->sfd, (struct sockaddr*)&sock->addr, sizeof(sock->addr));
+  bind(sock->sfd, (struct sockaddr *)&sock->addr, sizeof(sock->addr));
 }
-
 
 int unix_listen(struct unix_socket *sock) {
   if (DEBUG) {
-    DBG_PRINT("listening on %s, max connections: %d", sock->addr.sun_path, N_CONNECTIONS);
+    DBG_PRINT("listening on %s, max connections: %d", sock->addr.sun_path,
+              N_CONNECTIONS);
   }
   listen(sock->sfd, N_CONNECTIONS);
 }
@@ -43,6 +44,15 @@ int unix_connect(struct unix_socket *sock) {
   if (DEBUG) {
     DBG_PRINT("connecting to %s", sock->addr.sun_path);
   }
-  return connect(sock->sfd, (struct sockaddr*) &sock->addr, sizeof(sock->addr));
+  return connect(sock->sfd, (struct sockaddr *)&sock->addr, sizeof(sock->addr));
 }
 
+int unix_accept(struct unix_socket *srv_sock, struct unix_socket *client_sock) {
+  client_sock->sfd =
+      accept(srv_sock->sfd, (struct sockaddr *)&client_sock->addr,
+             &client_sock->addr_size);
+  if (DEBUG) {
+    DBG_PRINT("accepted connection from %s", client_sock->addr.sun_path);
+  }
+  return client_sock->sfd;
+}
--- os-lab-5/src/sockets.h
+++ os-lab-5/src/sockets.h
@@ -11,6 +11,7 @@ const char SRV_PATH[] = "unix_socket";
 
 struct unix_socket {
   struct sockaddr_un addr;
+  socklen_t addr_size;
   int sfd;
 };
 
@@ -18,3 +19,4 @@ struct unix_socket get_socket();
 int unix_bind(struct unix_socket *sock);
 int unix_listen(struct unix_socket *sock);
 int unix_connect(struct unix_socket *sock);
+int unix_accept(struct unix_socket *srv_sock, struct unix_socket *client_sock);
