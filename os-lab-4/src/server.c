abf03fb3a7d8af0717ea013dc4d1e9b613913f17
Freya Pines <one-true-cat@yandex.ru>
2025-10-10 16:23:04 +0300

    removed if-debug in server

--- os-lab-4/src/server.c
+++ os-lab-4/src/server.c
@@ -14,15 +14,11 @@ static int SHMID;
 
 void server_teardown(int signal) {
   SERVER_PRINT("stopping server");
-  if (DEBUG) {
-    DBG_PRINT("deleting semaphore set by key %d", SEMID);
-  }
+  DBG_PRINT("deleting semaphore set by key %d", SEMID);
 
   OR_DIE(semctl(SEMID, 0, IPC_RMID));
 
-  if (DEBUG) {
-    DBG_PRINT("marking mem segment %d for destruction", SHMID);
-  }
+  DBG_PRINT("marking mem segment %d for destruction", SHMID);
   OR_DIE(shmctl(SHMID, IPC_RMID, NULL));
 
   exit(EXIT_SUCCESS);
@@ -39,21 +35,14 @@ int main() {
 
   OR_DIE(SEMID = semget(SEM_KEY, 1, IPC_CREAT | 0600));
 
-  if (DEBUG) {
-    DBG_PRINT("created semaphore set %d", SEMID);
-  }
+  DBG_PRINT("created semaphore set %d", SEMID);
 
   OR_DIE(semctl(SEMID, 0, SETVAL, 1));
-  if (DEBUG) {
-    DBG_PRINT("intialized semaphore value to 1");
-  }
+  DBG_PRINT("intialized semaphore value to 1");
 
   int shmid;
   OR_DIE(SHMID = shmget(SHM_KEY, SHM_SIZE, IPC_CREAT | 0600));
-
-  if (DEBUG) {
-    DBG_PRINT("created shared memory of size %zu with id %d", SHM_SIZE, SHMID);
-  }
+  DBG_PRINT("created shared memory of size %zu with id %d", SHM_SIZE, SHMID);
 
   // Attach shared memory to addr space
   char* addr;
@@ -63,9 +52,7 @@ int main() {
   struct sembuf sem_wait = {0, 0, 0};
 
   while(1) {
-    if (DEBUG) {
-      DBG_PRINT("waiting for semaphore value to be set to 0");
-    }
+    DBG_PRINT("waiting for semaphore value to be set to 0");
     OR_DIE(semop(SEMID, &sem_wait, 1));
 
     SERVER_PRINT("got info: '%s'", addr);
