b2431c9c6c51302778841bb50419207d26dc70f3
Freya Pines <one-true-cat@yandex.ru>
2025-10-10 15:50:43 +0300

    add server setup and teardown

--- os-lab-4/src/server.c
+++ os-lab-4/src/server.c
@@ -1,11 +1,58 @@
 #include "common.h"
+#include "debug.h"
+#include "variables.h"
+#include "sys/sem.h"
+#include "sys/shm.h"
+#include <signal.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
 
 #define SERVER_PRINT(...) INFO_PRINT("server", __VA_ARGS__)
+
+static int SEMID;
+static int SHMID;
+
+void server_teardown(int signal) {
+  if (DEBUG) {
+    DBG_PRINT("deleting semaphore set by key %d", SEMID);
+  }
+
+  OR_DIE(semctl(SEMID, 0, IPC_RMID));
+
+  if (DEBUG) {
+    DBG_PRINT("marking mem segment %d for destruction", SHMID);
+  }
+  OR_DIE(shmctl(SHMID, IPC_RMID, NULL));
+
+  exit(EXIT_SUCCESS);
+}
 
 
 int main() {
   set_debug();
   SERVER_PRINT("starting server");
+
+  // Setup signals handling
+  struct sigaction sig = {&server_teardown};
+  sigaction(SIGINT, &sig, NULL);
+
+  OR_DIE(SEMID = semget(SEM_KEY, 1, IPC_CREAT));
+
+  if (DEBUG) {
+    DBG_PRINT("created semaphore set %d", SEMID);
+  }
+
+  int shmid;
+  OR_DIE(SHMID = shmget(SHM_KEY, SHM_SIZE, IPC_CREAT));
+
+  if (DEBUG) {
+    DBG_PRINT("created shared memory of size %zu with id %d", SHM_SIZE, SHMID);
+  }
+
+  while(1) {
+    
+  }
+  
 
   return 0;
 }
---
+++ os-lab-4/src/variables.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "stdlib.h"
+
+const int SEM_KEY = 1337;
+const int SHM_KEY = 420;
+const size_t SHM_SIZE = 1024;
